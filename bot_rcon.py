949b4c7586a03c4175f1d5878a2779bded3f9a9b
Luca <luca.lizaranzu@gmail.com>
2025-06-08 21:59:08 -0700

    Fixed role permission list

--- bot_rcon.py
+++ bot_rcon.py
@@ -3,18 +3,14 @@ from dotenv import load_dotenv
 import asyncio
 import os
 
-#Environment variables
+# Load environment variables
 load_dotenv()
 
 rcon_password = os.getenv('RCON_PASSWORD')
-
-#RCON
 RCON_HOST = "192.168.50.114"
 RCON_PORT = 25575
 
 async def call_mc_command(command: str) -> str | None:
-    loop = asyncio.get_event_loop()
-
     def run_rcon_command():
         try:
             with MCRcon(RCON_HOST, rcon_password, port=RCON_PORT) as mcr:
@@ -23,4 +19,4 @@ async def call_mc_command(command: str) -> str | None:
             print(f"[RCON Error] Failed to run command '{command}': {e}")
             return None
 
-    return await loop.run_in_executor(None, run_rcon_command)
+    return await asyncio.to_thread(run_rcon_command)
